fd3d940bc6b021f6ba8c1d61401f603b10a7a6ff
michaelrizzo2 <m_rizzo@live.com>
2015-05-06 15:47:58 -0400

    added get name method to Dog class and called get name method from Dog object

--- classes.rb
+++ classes.rb
@@ -5,7 +5,12 @@ class Dog
 		#This will be an instance variable for each instance of this class
 		@myname=name
 	end
+
+	def get_name
+		return @myname
+	end
 end
 
 my_dog=Dog.new
 my_dog.set_name("Michael")
+my_dog.get_name
