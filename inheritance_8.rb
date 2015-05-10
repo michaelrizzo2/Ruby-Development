db85c129e9432436d71eca0127e92c3c9d2c5e09
michaelrizzo2 <m_rizzo@live.com>
2015-05-10 12:46:21 -0400

    added getter and setter methods for name and description instance variables

--- inheritance_8.rb
+++ inheritance_8.rb
@@ -6,4 +6,21 @@ class Thing
 		@name=name
 		@description=description
 	end
+	#Setting up getter and setter methods for name and description
+	def get_name
+		return @name
+	end
+
+	def set_name(name)
+		@name=name
+	end
+
+	def get_description
+		return @description
+	end
+
+	def set_description(description)
+		@description=description
+	end
+
 end
