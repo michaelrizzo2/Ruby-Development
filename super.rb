7a434f4b2aeb8c4f07bf4d3bd68816aac1997c0a
michaelrizzo2 <m_rizzo@live.com>
2015-05-16 00:03:11 -0400

    Thing2 class set up

--- super.rb
+++ super.rb
@@ -8,6 +8,20 @@ class Thing
 
 	def amethod(aNewName)
 		@name=aNewName
-		puts "Thing.amethod:#{self.inspect}"
+		puts "Thing.amethod:#{self.inspect}\n\n"
 	end
+end
+
+class Thing2<Thing
+	def initialize(name,description)
+		super
+		@fulldescription="The name is #{@name} and the description is #{@description}"
+		puts "Thing2.initialize:#{self.inspect}\n\n"
+	end
+
+	def amethod(aNewName)
+		super(aNewName)
+		puts "Thing2.amethod:#{self.inspect}\n\n"
+	end
+
 end
