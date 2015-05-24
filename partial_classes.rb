29f094a19c2d6e5465fb9cef9966f6506c17d8fd
michaelrizzo2 <m_rizzo@live.com>
2015-05-24 18:42:38 -0400

    Partial classes A and B set up

---
+++ partial_classes.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+# We will demondtrate partial classes in ruby
+# We do not have to define the class all in one place
+#This is the first part of class A
+class A
+	def a
+		puts "a"
+	end
+end
+
+#This is the first part of class B 
+class B<A
+	def ba1
+		puts "ba1"
+	end
+end
+#
+#This is the second part of class A
+class A
+	def b 
+		puts "b"
+	end
+end
+
+#This is the second part of class B 
+class B<A
+	def ba2
+		puts "ba2"
+	end
+end
