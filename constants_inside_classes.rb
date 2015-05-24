768598f48d4378e6cc15de83faf3d555459d540a
michaelrizzo2 <m_rizzo@live.com>
2015-05-23 23:24:04 -0400

    Accessing a class inside a class using scope resolution

--- constants_inside_classes.rb
+++ constants_inside_classes.rb
@@ -7,3 +7,5 @@ end
 
 #We will now access a constant from the class using the scope resolution operator
 puts "#{X::A}"
+#A class inside a class is also considered a constant meaning we can do this
+puts "#{X::Y.new}"
