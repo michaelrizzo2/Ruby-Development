8cf80f985487aed80d9f9d7aecfa8bfa1b4a6d25
michaelrizzo2 <m_rizzo@live.com>
2015-05-25 23:50:22 -0400

    Learned how user defined string delimiters work

---
+++ user_defined_string_delimiters.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+#%Q/ and %/ is for double string quotes %q is for single string quotes
+puts %Q/This is a double string/
+puts %/This is a double string/
+puts %q/This is a single string/
