bbabf6081f4bf2d1f87fcf77dcb47c4f25c656f3
michaelrizzo2 <m_rizzo@live.com>
2015-05-26 19:53:13 -0400

    Demonstrated string concatenation in ruby 3 ways

---
+++ string_concatenation.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+#String concatenation can be done in ruby using <<,+ or a blank space 
+puts "String concatenation in ruby using <<"
+s="Hello"<<"World"
+puts s 
+puts "String concatenation in ruby using +"
+s="Hello" + "World"
+puts s 
+puts "String concatenation in ruby using a blank space"
+s="Hello"  "World"
+puts s 
+
