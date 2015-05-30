e0a0f9896dbe645b70231d1e3ba57bfdfe4d7558
michaelrizzo2 <m_rizzo@live.com>
2015-05-30 12:30:54 -0400

    Showing string slicing using two dot range notation in ruby

--- string_slicing.rb
+++ string_slicing.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 # This will demonstrate string slicing
 s="Hello world"
-puts "Slicing string using comma"
-puts s[1,3]
+#puts "Slicing string using comma"
+#puts s[1,3]
+puts "Slicing string using two dot range notation"
+puts s[1..3]
