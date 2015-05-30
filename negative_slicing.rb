7c29352c9809ffd267a58307ba9d01083251c1f5
michaelrizzo2 <m_rizzo@live.com>
2015-05-30 13:08:13 -0400

    Showing string slicing with negative indices using comma in ruby

---
+++ negative_slicing.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+#This will show how to use negative indexing in ruby with the comma and the two dot range notation
+s="Hello world"
+puts "array slicing with negative indices using the comma notation"
+puts s[-1,1]
+puts s[-5,5]
+
