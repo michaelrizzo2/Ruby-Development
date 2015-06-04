0ce7c5007916b3a9db4a65734e50e240fadbadf5
michaelrizzo2 <m_rizzo@live.com>
2015-06-03 23:02:37 -0400

    Showing how to create an array of single quoted strings using string_array_creator.rbw in ruby

---
+++ string_array_creator.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+#This will show how to create  an array full of single strings using %w
+y=%w(i am an array of single strings)
+puts "Showing y output"
+puts y
+puts "Showing type of y output"
+puts y.class()
