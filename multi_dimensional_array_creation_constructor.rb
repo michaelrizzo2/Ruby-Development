00da48597f15994008d631b54a4f1418c16b45d4
michaelrizzo2 <m_rizzo@live.com>
2015-06-05 15:51:27 -0400

    multidimensional array filled using array constructor in ruby

--- multi_dimensional_array_creation_constructor.rb
+++ multi_dimensional_array_creation_constructor.rb
@@ -2,3 +2,6 @@
 #creating array with two slots which will each hold an array
 my_array=Array.new(2)
 puts "my array is #{my_array}"
+my_array[0]=Array.new(2,"Hello")
+my_array[1]=Array.new(2,"world")
+puts "my array is #{my_array}"
