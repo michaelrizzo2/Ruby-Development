427a1b1ac4df3e1f3cd244e43802f238190dc02f
michaelrizzo2 <m_rizzo@live.com>
2015-06-04 23:13:13 -0400

    Showing how to create an array of 2 Hello World  elements using the array constructor in ruby

--- array_creation_constructor.rb
+++ array_creation_constructor.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 #my_array=Array.new
 #puts "My array is #{my_array}"
-my_array=Array.new(2)
+#my_array=Array.new(2)
+#puts "My array is #{my_array}"
+my_array=Array.new(2,"Hello World")
 puts "My array is #{my_array}"
