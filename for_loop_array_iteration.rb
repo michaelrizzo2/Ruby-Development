1eb3ee639d47033d5db920b02783ba592db59bd6
michaelrizzo2 <m_rizzo@live.com>
2015-06-06 15:20:25 -0400

    each element multidimensional array iterated through using for  in loop in ruby(good way)

--- for_loop_array_iteration.rb
+++ for_loop_array_iteration.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/ruby
 #we will iterate through an multidimensional array with an for loop
 array=[[1,2,3,4],[5,6,7,8]]
-for entry in array
-	puts "entry in array is #{entry}"
+#for entry in array
+#	puts "entry in array is #{entry}"
+#end
+#we will iterate through the array four entries at a time using an for in loop
+for (a,b,c,d) in array
+	puts "a is #{a} and b is #{b} and c is #{c} and d is #{d}"
 end
