7ce6c4b656f3bc393f9247390e709ea848c38897
michaelrizzo2 <m_rizzo@live.com>
2015-06-07 23:05:31 -0400

    Showing how to use slice an array in ruby using the number of characters from the starting index

---
+++ array_index.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+#This will show how to index through an array
+array=["h","e","l","l","o"," ","w","o","r","l","d"]
+puts "using the number of chracters from the starting index to slice the array"
+puts "First 5 letters are #{array[0,5]}"
+puts "Last 5 letters are #{array[-5,5]}"
