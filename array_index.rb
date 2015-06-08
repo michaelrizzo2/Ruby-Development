9e803959f16004edd29b5a84c2d5fd4f0e255312
michaelrizzo2 <m_rizzo@live.com>
2015-06-07 23:28:26 -0400

    Showing how to use slice an array in ruby using the two dot range notation

--- array_index.rb
+++ array_index.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/ruby
 #This will show how to index through an array
 array=["h","e","l","l","o"," ","w","o","r","l","d"]
-puts "using the number of chracters from the starting index to slice the array"
-puts "First 5 letters are #{array[0,5]}"
-puts "Last 5 letters are #{array[-5,5]}"
+#puts "using the number of characters from the starting index to slice the array"
+#puts "First 5 letters are #{array[0,5]}"
+#puts "Last 5 letters are #{array[-5,5]}"
+puts "using the two dot range notation to slice the array"
+puts "First 5 letters are #{array[0..4]}"
+puts "Last 5 letters are #{array[-5..-1]}"
