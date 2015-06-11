3766cb67a234c2219218516496536dfad161f2ba
michaelrizzo2 <m_rizzo@live.com>
2015-06-10 23:45:25 -0400

    fixed errors in class setup, set up objects,and showed the result of the comparision using the overloaded array comparison method

--- comparision_overriding.rb
+++ comparision_overriding.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/ruby
 #This will show how to override the comparison method by for specific object types by including modules
 
-class myArray<Array
+class MyArray < Array
 	#This will help us override the array comparison method
-	include comparable
+	include Comparable
 
 	def <=> (other_array)
 		self.length<=>other_array.length
 	end
 end
+
+myarray1=MyArray.new([0,1,2,3])
+myarray2=MyArray.new([1,2,3,4])
+puts "Comparison result is #{myarray1 <=> myarray2}"
