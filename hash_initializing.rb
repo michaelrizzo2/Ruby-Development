86d8388feafefaafb8c6810450f48374348c3276
michaelrizzo2 <m_rizzo@live.com>
2015-06-12 23:33:17 -0400

    Showed how to initialize a hash and add keys and values to the hash in ruby

---
+++ hash_initializing.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#We will see how to initialize a hash(dictionary) in ruby and to make assignments to the dictionary
+my_hash=Hash.new
+my_hash["Treasure1"]=1
+my_hash["Treasure2"]=2
+my_hash["Treasure3"]=3
+my_hash["Treasure4"]=4
+puts "my hash is #{my_hash}"
