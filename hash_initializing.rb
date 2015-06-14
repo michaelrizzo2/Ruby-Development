3d567b562c2c7c7b402b031051449685e06855bd
michaelrizzo2 <m_rizzo@live.com>
2015-06-13 22:51:14 -0400

    Showed how you can initialize a hash in ruby inside brackets using key=>value

--- hash_initializing.rb
+++ hash_initializing.rb
@@ -6,3 +6,6 @@ my_hash["Treasure2"]=2
 my_hash["Treasure3"]=3
 my_hash["Treasure4"]=4
 puts "my hash is #{my_hash}"
+#This will initialize a hash using key=>value inside curly brackets
+my_new_hash={"name"=>"Michael","age"=>26,"Location"=>"Naples FL"}
+puts "my new hash is #{my_new_hash}"
