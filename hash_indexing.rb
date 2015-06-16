04fc2960a71386360d0243065c58601439a8288c
michaelrizzo2 <m_rizzo@live.com>
2015-06-15 21:14:20 -0400

    Showed how to access a hash value using keys in ruby

---
+++ hash_indexing.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#This will show the concept of hash indexing
+my_new_hash={"name"=>"Michael","age"=>26,"Location"=>"Naples FL"}
+#Hash indexing is when you retrieve the value of a hash based on the key
+puts "name is #{my_new_hash["name"]}"
+puts "iq is #{my_new_hash["iq"]}"
+
+
