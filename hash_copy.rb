65074b5ee213d6706db65da14041a8f6d094575b
michaelrizzo2 <m_rizzo@live.com>
2015-06-15 23:03:57 -0400

    Showed how to clone  one hash to another in ruby

--- hash_copy.rb
+++ hash_copy.rb
@@ -2,6 +2,10 @@
 #This will show the results of setting one hash equal to another and cloning the same hash
 my_old_hash={"name"=>"Michael","age"=>26,"Location"=>"Naples FL"}
 #Setting one hash equal to another
-my_new_hash=my_old_hash
-my_old_hash["name"]="Michael Rizzo"
+#my_new_hash=my_old_hash
+#my_old_hash["name"]="Michael Rizzo"
+#puts "the original hash is #{my_old_hash} and the new hash is #{my_new_hash}"
+#cloning one hash to another
+my_new_hash=my_old_hash.clone
+my_new_hash["name"]="Michael Rizzo"
 puts "the original hash is #{my_old_hash} and the new hash is #{my_new_hash}"
