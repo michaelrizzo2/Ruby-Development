4cef078d98f8e38037636034aab26937c02b245b
michaelrizzo2 <m_rizzo@live.com>
2015-06-16 23:31:53 -0400

    hash sorting using block inside function(still not working)

---
+++ hash_sort.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+#This will show how to sort a hash
+my_new_hash={1=>"Michael",3.16=>"Gpa","Address"=>"6284 Towncenter Circle"}
+#this function will sort the hash by key(a being the key,b being the value)
+def hash_sorter(my_hash)
+	return my_hash.sort
+	{
+		|a,b| 
+
+		a.to_s<=>b.to_s
+	}
+end
+
+puts "sorted hash is #{hash_sorter(my_new_hash)}"
+
