87de4d407099cd5ca5312a84219d484a77497a46
michaelrizzo2 <m_rizzo@live.com>
2015-06-18 22:26:14 -0400

    for loop created to iterate through an array in ruby

---
+++ for_loop.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+#This will show a python style for loop iterating through a list without the colon and including the end syntax from octave and the do syntax from fortran
+
+for i in [1,2,3] do 
+	puts "The number is #{i}"
+end
