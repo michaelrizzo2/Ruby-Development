4c0e56ae5e25f6943d92ecaee01a4657bb3e2a97
michaelrizzo2 <m_rizzo@live.com>
2015-06-20 22:54:38 -0400

    showing one line for loop in ruby

---
+++ for_loop_do.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+#This will show the difference between a one line for loop and a multiline for loop
+
+for s in [1,2,3] do puts "Entry is #{s}" end
+
+#do keyword is required for one line for loop statement 
