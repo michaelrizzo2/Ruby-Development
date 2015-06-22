f258be2f215cad3b064f6f67b666321f498f25b1
michaelrizzo2 <m_rizzo@live.com>
2015-06-21 23:18:40 -0400

    showing range based each iterator needs parentheses in ruby

--- range_based_loops.rb
+++ range_based_loops.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 #This will show how you need parentheses for the range of an each loop and not for a for loop
-for s in 1..3
+#for s in 1..3
+#	puts "s is #{s}"
+#end
+
+(1..3).each do |s|
 	puts "s is #{s}"
 end
