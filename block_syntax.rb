ef79a71d9e32d768a11beff2a08f3e83dc489d16
michaelrizzo2 <m_rizzo@live.com>
2015-06-22 22:53:58 -0400

    showing a curly braced block in an each iterator in ruby

--- block_syntax.rb
+++ block_syntax.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
 #The first example will show a do end block without curly brackets
-[[1,2,3],[4,5,6],[7,8,9]].each do 
+#[[1,2,3],[4,5,6],[7,8,9]].each do 
+#	|a,b,c|
+#	puts "A is #{a} and B is #{b} and C is #{c}"
+#end
+#This example will show an curly brace block 
+[[1,2,3],[4,5,6],[7,8,9]].each {
 	|a,b,c|
 	puts "A is #{a} and B is #{b} and C is #{c}"
-end
+} 
