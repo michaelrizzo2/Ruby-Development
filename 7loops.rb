6e3bda363f3695d47004484f822d1a6b6b426d04
michaelrizzo2 <m_rizzo@live.com>
2015-06-24 22:56:24 -0400

    set up tired function in ruby for while statement

---
+++ 7loops.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+#This will show all of the alternative while sytaxes in ruby(i hope)
+$hours_asleep=0
+
+def tired
+	if $hours_asleep>=8 then 
+		$hours_asleep=0
+		return false
+	else
+		$hours_asleep+=1
+		return true
+	end 
+end
+
