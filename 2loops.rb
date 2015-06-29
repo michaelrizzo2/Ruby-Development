444fb3874429b09c0e1b5c30aebad0ce448e2e03
michaelrizzo2 <m_rizzo@live.com>
2015-06-28 23:24:23 -0400

    showed a while condition do block end loop that does not execute

---
+++ 2loops.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+#This will show 2 while loops that do not work and one while loop that runs at least once
+x=100
+
+#This loop will fail to execute
+while x<100 do puts "Success" end
+
