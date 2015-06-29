422da79904b16c4927d5335fc2a0da1190d98827
michaelrizzo2 <m_rizzo@live.com>
2015-06-29 10:43:58 -0400

    showed do while loop disguised as a begin block end while condition

--- 2loops.rb
+++ 2loops.rb
@@ -3,5 +3,12 @@
 x=100
 
 #This loop will fail to execute
-#while x<100 do puts "Success" end
+while x<100 do puts "Success" end
+
+#This loop will fail to execute
+puts "Success" while x<100  
+
+#This will be the do while loop that will execute at least once
+begin puts "Success" end while x<100
+
 
