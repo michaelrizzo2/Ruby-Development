a0833f5df072d2c1031edc01ae2a49abc9a2c6b3
michaelrizzo2 <m_rizzo@live.com>
2015-06-30 22:10:37 -0400

    Iterating through an array using an while loop in ruby

---
+++ array_iterator.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+#This will iterate through an array using an until loop and while loop
+array=[1,2,3,4,5]
+
+#This will set the iterator index for the array
+index=0
+
+while index<array.length
+	puts "array entry #{index} is #{array[index]}"
+	index+=1
+end
