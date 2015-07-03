9adc35d7ac964e1c50a4189c7eb251ad97650c86
michaelrizzo2 <m_rizzo@live.com>
2015-07-02 22:32:20 -0400

    Iterating through an array using an  loop(do end style) with a break statement inside an if loop in ruby

---
+++ 3loops.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+#This will show how to use the loop keyword in ruby to iterate through an array
+array=[1,2,3,4,5]
+index=0
+#This will use the loop do end syntax
+loop do
+	puts "Array number is #{index} and entry is #{array[index]}" 
+	index+=1
+	if index==array.length
+		break
+	end
+end
