e799301697dc1888127f4bd6e4fb62cb369dc529
michaelrizzo2 <m_rizzo@live.com>
2015-07-03 23:18:28 -0400

    Added else loop to if else loop in ruby

--- if_else.rb
+++ if_else.rb
@@ -5,4 +5,7 @@ day_worked="Saturday"
 if day_worked.downcase=="saturday" then
 	day_type="weekend"
 	puts "#{day_worked} is a #{day_type}"
+else
+	day_type="weekday"
+	puts "#{day_worked} is a #{day_type}"
 end
