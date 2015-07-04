a93459da835dc735debc30f10178be7241c06c85
michaelrizzo2 <m_rizzo@live.com>
2015-07-03 23:31:52 -0400

    improper one line if else loop with then keyword removed in ruby

--- one_line_if_loops.rb
+++ one_line_if_loops.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
 #This will show the proper way to do one line if else statement in ruby
 x=1
-if x==1 then puts "OK" else puts "not ok" end
+#if x==1 then puts "OK" else puts "not ok" end
+if x==1 puts "OK" else puts "not ok" end
