89e00e6636496996564d1054b9c98671af9936bf
michaelrizzo2 <m_rizzo@live.com>
2015-07-03 23:16:15 -0400

    showing basic if else loop in ruby

---
+++ if_else.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#This will show a basic if else loop
+day_worked="Saturday"
+
+if day_worked.downcase=="saturday" then
+	day_type="weekend"
+	puts "#{day_worked} is a #{day_type}"
+end
