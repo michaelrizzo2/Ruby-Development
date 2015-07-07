b92369c3bbf6343b011f86b8d23e2ecf7c9d9080
michaelrizzo2 <m_rizzo@live.com>
2015-07-06 23:27:37 -0400

    proper boolean logic used in if statement

--- and_or_wrong.rb
+++ and_or_wrong.rb
@@ -2,7 +2,7 @@
 #This will show how not to use the and or keywords in ruby
 aday="saturday"
 working_overtime=true
-if aday=="saturday" or aday=="sunday" and not working_overtime
+if aday=="saturday" or (aday=="sunday" and not working_overtime)
 	daytype="holiday"
 	puts "#{aday} is a #{daytype}"
 else
