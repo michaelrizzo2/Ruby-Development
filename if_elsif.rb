176244749f790737d67ff5f197e5510f0fd74515
michaelrizzo2 <m_rizzo@live.com>
2015-07-07 23:13:28 -0400

    Showing how to use an if elsif else loop inside a while loop in ruby(negative values bug)

---
+++ if_elsif.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+#This will show how to use an if elsif else loop
+input=nil
+while input !="q" do
+	puts "Please input number between 1 and 1000"
+	input=gets().chomp()
+	if input =="q"
+		puts "quitting"
+		break
+	elsif input.to_i>800
+		puts "Rate of pay too high"
+	elsif input.to_i<=800
+		puts "We can afford that"
+	end
+end
+
