2e06b5308faf8eb170b15524958fe623ba68b671
michaelrizzo2 <m_rizzo@live.com>
2015-07-13 23:16:59 -0400

    showed how to use unless with conditional statements

---
+++ unless_conditional.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+#this will show how to use conditionals with the unless(if not) statement
+aday="Saturday"
+unless aday=="Saturday" or aday=="Sunday" 
+	puts "weekday"
+else
+	puts "weekend"
+end
