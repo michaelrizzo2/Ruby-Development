29e9ff1e3a795b93dd79763e65feebede9651d34
michaelrizzo2 <m_rizzo@live.com>
2015-07-17 22:25:23 -0400

    output set for if unless modifier in ruby

--- if_unless_modifier.rb
+++ if_unless_modifier.rb
@@ -1,5 +1,25 @@
 #!/usr/bin/ruby
 #This will show how to use if and unless modifiers in ruby
+#This will hold all of the functions
+$hours_asleep=0
+
+def tired
+	if $hours_asleep>=8 then
+		$hours_asleep=0
+		return false
+	else
+		$hours_asleep+=1
+		return true
+		end
+end
+
+def snore
+	puts "Snore"
+end
+
+def sleep                                                                                     
+	puts "z"*$hours_asleep                                                                
+end                                           
 sleep if tired
 
 begin
