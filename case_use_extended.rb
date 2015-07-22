4d92ab86b407f8810cd9565a290d5cd8a4b5c0bd
michaelrizzo2 <m_rizzo@live.com>
2015-07-21 23:16:02 -0400

    case statement using day as a condition in ruby extended with if statements inside when condition 6..7

---
+++ case_use_extended.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+#This will show how to use the case statement in ruby with (begin..end) range syntax in ruby
+day=1
+case(day)
+	when 1 then puts "It's Monday"
+	when 2 then puts "It's Tuesday"
+	when 3 then puts "It's Wednesday"
+	when 4 then puts "It's Thursday"
+	when 5 then puts "It's Friday"
+		puts "Nearly the weekend"
+	when (6..7) then puts "It is the weekend"
+		puts "It is saturday" if i==6
+		puts "It is sunday" if i==7
+		puts "Yeah it is the weekend"
+	else puts "You have not chosen a real day"
+end
